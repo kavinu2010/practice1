d629b9d67fee08066aa19c9040e14722f6cdf7ad
kavitha <kavinu2010@gmail.com>
2024-12-18 09:16:14 +0100

    commented

---
+++ lambda.py
@@ -0,0 +1,37 @@
+'''square=lambda x: x**2
+print(square(5))
+
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+even=list(filter(lambda x: x%2==0, numbers))
+print(even)
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+even=list(map(lambda x: x%2==0, numbers))
+print(even)'''
+
+# def arrayCheck(number):
+#   for i in number:
+#     if(i==1 and i+1==2 and i+2==3):
+#       print(True)
+    
+
+     
+
+# arrayCheck([1,2,4,4,5])
+
+# def arrayCheck(number):
+#   for i in number:
+#     if(i==1 and i+1==2 and i+2==3):
+#       print(True)
+    
+
+     
+
+# arrayCheck([1,2,4,4,5])
+
+
+
+
+
+
